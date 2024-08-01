bdaeb91d30f0f91c076edd42ad92f2c32e7b6e42
Sandz0814 <sandrojimena0814@gmail.com>
2024-08-02 03:57:15 +0800

    13

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -11,10 +11,10 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope="class")
 def setup(request):
     # file = "C:\\Users\\Change Me\\Downloads\\chromedriver-win64 (3)\\chromedriver-win64\\chromedriver.exe"
-    file = "./chromedriver.exe"
-    service = ChromeService(executable_path=file)
-    driver = webdriver.Chrome(service=service)
-    # driver = webdriver.Chrome()
+    # file = "./chromedriver.exe"
+    # service = ChromeService(executable_path=file)
+    # driver = webdriver.Chrome(service=service)
+    driver = webdriver.Edge()
     driver.get('https://admin-demo.nopcommerce.com/login')
     driver.implicitly_wait(10)
     driver.maximize_window()
