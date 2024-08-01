c6dce9882d3e67094d5301d4f67a1ae01c10d2df
Sandz0814 <sandrojimena0814@gmail.com>
2024-08-02 03:38:02 +0800

    install latest chromedriver

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -1,14 +1,19 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common import options
+# from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="class")
 def setup(request):
-    # Initialize Chrome options
-    # options = Options()
-    # options.add_argument("--headless")
-    # driver = webdriver.Edge(options=options)
-    driver = webdriver.Chrome()
+    file = "C:\\Users\\Change Me\\Downloads\\chromedriver-win64 (3)\\chromedriver-win64\\chromedriver.exe"
+    service = ChromeService(executable_path=file)
+    driver = webdriver.Chrome(service=service)
+    # driver = webdriver.Chrome()
     driver.get('https://admin-demo.nopcommerce.com/login')
     driver.implicitly_wait(10)
     driver.maximize_window()
