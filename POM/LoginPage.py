981bd99726720025523aecaf415ee40ada203054
Sandz0814 <sandrojimena0814@gmail.com>
2024-08-02 02:00:35 +0800

    furnish2

--- POM/LoginPage.py
+++ POM/LoginPage.py
@@ -9,6 +9,18 @@ class LoginPages:
     login_btn = "//button[normalize-space()='Log in']"
     logout_text = "//a[normalize-space()='Logout']"
 
+    success_login_message = "//a[normalize-space()='Logout']"
+    invalid_email_message = "//li[normalize-space()='No customer account found']"
+    invalid_password_message = "//li[normalize-space()='The credentials provided are incorrect']"
+    invalid_email_format_message = "//span[@class='field-validation-error']"
+    invalid_no_email_input = "//span[@id='Email-error']"
+
+    expected_success_login = 'Logout'
+    expected_invalid_password = 'The credentials provided are incorrect'
+    expected_invalid_email = 'No customer account found'
+    expected_invalid_email_format = 'Please enter a valid email address.'
+    expected_no_email_input = 'Please enter your email'
+
     def __init__(self, driver):
         self.driver = driver
 
@@ -30,3 +42,23 @@ class LoginPages:
     def verify_login_if_successful(self):
         logout_text = self.driver.find_element(By.XPATH, self.logout_text).text
         return logout_text
+
+    def verify_success_login_message(self):
+        return self.driver.find_element(By.XPATH, self.success_login_message).text
+
+    def verify_invalid_email_message(self):
+        return self.driver.find_element(By.XPATH, self.invalid_email_message).text
+
+    def verify_invalid_password_message(self):
+        return self.driver.find_element(By.XPATH, self.invalid_password_message).text
+
+    def verify_invalid_email_format_message(self):
+        return self.driver.find_element(By.XPATH, self.invalid_email_format_message).text
+
+    def verify_invalid_no_email_input(self):
+        return self.driver.find_element(By.XPATH, self.invalid_no_email_input).text
+
+
+
+
+
--- TestCases/test_login2.py
+++ TestCases/test_login2.py
@@ -6,13 +6,6 @@ from TestData.data import TestData
 
 @pytest.mark.usefixtures("setup")
 class TestLogin:
-
-    expected_success_login = 'Logout'
-    expected_invalid_password = 'The credentials provided are incorrect'
-    expected_invalid_email = 'No customer account found'
-    expected_invalid_email_format = 'Please enter a valid email address.'
-    expected_no_email_input = 'Please enter your email'
-
     def test_login(self, setup):
         self.driver = setup
         self.login = LoginPages(self.driver)
@@ -25,6 +18,31 @@ class TestLogin:
             self.login.verify_input_email(user['email'])
             self.login.verify_input_password(user['password'])
             self.login.verify_login_btn()
+
+            if user['expected'] == LoginPages.expected_invalid_email:
+                assert LoginPages.expected_invalid_email in self.login.verify_invalid_email_message()
+                print('Negative test with invalid Email and valid Password . Passed!')
+
+            elif user['expected'] == LoginPages.expected_invalid_password:
+                assert LoginPages.expected_invalid_password in self.login.verify_invalid_password_message()
+                print('Negative test with valid Email and invalid Password . Passed!')
+
+            elif user['expected'] == LoginPages.expected_invalid_email_format:
+                assert LoginPages.expected_invalid_email_format in self.login.verify_invalid_email_format_message()
+                print('Negative test with invalid Email format. Passed!')
+
+            elif user['expected'] == LoginPages.expected_no_email_input:
+                assert LoginPages.expected_no_email_input in self.login.verify_invalid_no_email_input()
+                print('Negative test with no email. Passed!')
+
+            elif user['expected'] == LoginPages.expected_success_login:
+                assert LoginPages.expected_success_login in self.login.verify_success_login_message()
+                print('Positive test with valid Email and valid Password . Passed!')
+
+
+
+
+
 
 
 
